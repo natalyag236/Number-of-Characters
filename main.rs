c44a245b912edf3baa7837160830bba35cae2e8c
natalyag236 <142938978+natalyag236@users.noreply.github.com>
2024-09-24 16:13:19 -0400

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,42 @@
+use std::collections::HashMap;
+use std::io::{self,Write};
+
+// the main exeutes the chararcter counting 
+pub fn main() {
+    let mut input = String::new();
+    print!("Enter a string: ");
+    io::stdout().flush().unwrap(); 
+
+   // read the users input from the input var
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read user input ");
+       
+    // counter for the toatl num of characters 
+    let mut counter: i32 = 0;
+    // the hashmap stores the character count 
+    let mut char_count: HashMap<char, i32> = HashMap::new(); 
+    // goes over each character from the input string 
+    for ch in input.chars() {
+        counter += 1;
+
+        let count: &mut i32 = char_count.entry(ch).or_insert(0);
+        *count += 1;
+    }
+
+    let mut max_char: char = ' ';// var to hold the most frequent characters
+    let mut max_count: i32 = 0; // var to hold the total num of characters
+// goes over the characters to find the most frequent character
+    for (ch, count) in &char_count {
+        if *count > max_count {
+            max_count = *count;
+            max_char = *ch;
+        }
+    }
+    // Print the toatl numbers of characters 
+    println!("Total num of characters: {}", counter); 
+    // Prints the most frequent characters and how many times it appears 
+    if max_count > 1 {
+        println!("The most frequent occurring character is '{}' and appears {} times.", max_char, max_count);
+    }
+}
