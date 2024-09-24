31e4ef56cfa17e4ce09bac12867b80cf5d5ced6c
Natalya Goldsmith <natalya@MacBook-Pro-6.local>
2024-09-24 16:08:22 -0400

    Your commit message here

---
+++ main1/target/debug/deps/main1-5e82c416d0636868.d
@@ -0,0 +1,5 @@
+/Users/natalya/Rust/main1/target/debug/deps/libmain1-5e82c416d0636868.rmeta: src/main.rs
+
+/Users/natalya/Rust/main1/target/debug/deps/main1-5e82c416d0636868.d: src/main.rs
+
+src/main.rs:
---
+++ main1/target/debug/deps/main1-96f4e2c61f5426dd.d
@@ -0,0 +1,5 @@
+/Users/natalya/Rust/main1/target/debug/deps/libmain1-96f4e2c61f5426dd.rmeta: src/main.rs
+
+/Users/natalya/Rust/main1/target/debug/deps/main1-96f4e2c61f5426dd.d: src/main.rs
+
+src/main.rs:
