aacddf12f90be2502a3ee491803769a521459a6c
Natalya Goldsmith <natalya@MacBook-Pro-6.local>
2024-09-24 23:21:51 -0400

    Your commit message

--- main1/src/main.rs
+++ main1/src/main.rs
@@ -1,40 +1,41 @@
+
 use std::collections::HashMap;
 use std::io::{self,Write};
 
 // the main exeutes the chararcter counting 
 pub fn main() {
-    let mut input = String::new();
+    let mut user_input = String::new();
     print!("Enter a string: ");
     io::stdout().flush().unwrap(); 
 
    // read the users input from the input var
     io::stdin()
-        .read_line(&mut input)
+        .read_line(&mut user_input)
         .expect("Failed to read user input ");
        
    
     // the hashmap stores the character count 
     let mut char_count: HashMap<char, i32> = HashMap::new(); 
     // goes over each character from the input string 
-    for ch in input.chars() {
+    for ch in user_input.chars() {
         let count: &mut i32 = char_count.entry(ch).or_insert(0);
         *count += 1;
     }
 
-    let mut max_char: char = ' ';// var to hold the most frequent characters
-    let mut max_count: i32 = 0; // var to hold the total num of characters
+    let mut repeating_char: char = ' ';// var to hold the most frequent characters
+    let mut num_char: i32 = 0; // var to hold the total num of characters
 // goes over the characters to find the most frequent character
     for (ch, count) in &char_count {
-        if *count > max_count {
-            max_count = *count;
-            max_char = *ch;
+        if *count > num_char {
+            num_char = *count;
+            repeating_char = *ch;
         }
     }
     
     
     // Prints the most frequent characters and how many times it appears 
-    if max_count > 1 {
-        println!("The most frequent occurring character is '{}' and appears {} times.", max_char, max_count);
+    if num_char > 1 {
+        println!("The most frequent occurring character is '{}' and appears {} times.", repeating_char, num_char);
     }else{
         print!("No repeating characters")
     }
